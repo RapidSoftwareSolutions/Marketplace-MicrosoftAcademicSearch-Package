74b386367b1b9909d83cdc9fb13ab67b4226a44f
ivz-dev <zakutynsky@yahoo.com>
2017-10-10 17:55:25 +0300

    fixed

--- src/routes/graphSearch.php
+++ src/routes/graphSearch.php
@@ -4,7 +4,7 @@ $app->post('/api/MicrosoftAcademicSearch/graphSearch', function ($request, $resp
 
     $settings = $this->settings;
     $checkRequest = $this->validation;
-    $validateRes = $checkRequest->validate($request, ['key','region','mode','body']);
+    $validateRes = $checkRequest->validate($request, ['key','region','body']);
 
     if(!empty($validateRes) && isset($validateRes['callback']) && $validateRes['callback']=='error') {
         return $response->withHeader('Content-type', 'application/json')->withStatus(200)->withJson($validateRes);
@@ -12,11 +12,9 @@ $app->post('/api/MicrosoftAcademicSearch/graphSearch', function ($request, $resp
         $post_data = $validateRes;
     }
 
-    $requiredParams = ['key'=>'key','region'=>'region','mode'=>'mode','body'=>'body'];
+    $requiredParams = ['key'=>'key','region'=>'region','body'=>'body'];
     $optionalParams = [];
-    $bodyParams = [
-       'query' => ['mode']
-    ];
+    $bodyParams = [];
 
     $data = \Models\Params::createParams($requiredParams, $optionalParams, $post_data['args']);
 
