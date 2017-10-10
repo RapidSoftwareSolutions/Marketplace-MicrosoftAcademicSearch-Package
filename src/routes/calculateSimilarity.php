b89d160031f96c0b55a3543a04de2efe4f63bb6d
ivz-dev <zakutynsky@yahoo.com>
2017-10-10 17:16:43 +0300

    fixed

--- src/routes/calculateSimilarity.php
+++ src/routes/calculateSimilarity.php
@@ -37,7 +37,7 @@ $app->post('/api/MicrosoftAcademicSearch/calculateSimilarity', function ($reques
 
         if(in_array($resp->getStatusCode(), ['200', '201', '202', '203', '204'])) {
             $result['callback'] = 'success';
-            $result['contextWrites']['to'] = is_array($responseBody) ? $responseBody : json_decode($responseBody);
+            $result['contextWrites']['to']['result'] = is_array($responseBody) ? $responseBody : json_decode($responseBody);
             if(empty($result['contextWrites']['to'])) {
                 $result['contextWrites']['to']['status_msg'] = "Api return no results";
             }
